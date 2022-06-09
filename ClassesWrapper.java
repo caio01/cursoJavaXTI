99e6d6701a72f2c33404c12844d5879c1ed561b3
Caio Oliveira <caioinacio00@gmail.com>
2022-06-09 13:53:03 -0300

    final version

---
+++ ClassesWrapper.java
@@ -0,0 +1,23 @@
+/**
+ * @author Caio Oliveira
+ * Aula 010 - Curso Java XTI
+ */
+
+public class HelloWorld{
+
+    public public static void main(String[ args]) {
+        Integer idade = new Integer(31);
+        Double preco = new Double("12.45");
+        int i = preco.intValue();
+        byte b = preco.byteValue();
+        Boolean casado = new Boolean("yes");
+
+        double d1 = Double.parseDouble("123.45");
+        int i1 = Integer.parseInt("123");
+        float f1 = Float.parseFloat("3.14F");
+
+        int i2 = Integer.valueOf("101011", 2);
+
+    }
+} 
+
---
+++ Desafio_IMC.java
@@ -0,0 +1,30 @@
+import javax.swing.JOptionPane;
+
+/**
+ * @author Caio Oliveira
+ * Aula 019 - Curso Java XTI
+ * Desafio IMC - Calcular IMC
+ * 
+ * IMC = pesoEmQuilogramas / (alturaEmMetros * alturaEmMetros)
+ */
+
+
+public class Desafio_IMC {
+    public static void main(String[] args){
+        
+        String peso = JOptionPane.showInputDialog("Qual seu peso em kg?");
+        String altura = JOptionPane.showInputDialog("Qual sua altura em m?");
+        
+        double pesoEmQuilogramas = Double.parseDouble(peso);
+        double alturaEmMetros = Double.parseDouble(altura);
+        double imc = pesoEmQuilogramas / (alturaEmMetros * alturaEmMetros);
+
+        String msg = (imc >= 20 && imc <= 25) ? "Peso ideal" : "Fora do peso"; 
+        msg = "IMC = " + imc + "\n" + msg;  
+        
+        JOptionPane.showMessageDialog(null, msg);
+        //System.out.println("IMC = " + imc);
+        //System.out.println(msg);
+
+    }
+}
