f481b507310288f59a97b6fe3b60f14043b3694d
Caio Oliveira <caioinacio00@gmail.com>
2022-06-17 12:51:36 -0300

    part. 3

--- part3/Arquivo.java
+++ part3/Arquivo.java
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-package part3;
-/**
- * @author Caio Oliveira
- * Aula 062 - Curso Java XTI
-*/
-
-=======
 /**
  * @author Caio Oliveira
  * Aula 063 - Curso Java XTI
@@ -18,24 +10,10 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
->>>>>>> de702cc5571a04cba038bc8616481ef3cf474c67
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Arquivo {
-<<<<<<< HEAD
-    public static void main(String[] args){
-
-        Path path = Paths.get("C:/Users/cinacio/Documents/cursoJavaXTI/part3");
-        System.out.println(path.toAbsolutePath());
-        System.out.println(path.getParent());
-        System.out.println(path.getRoot());
-        System.out.println(path.getFileName());
-
-        //CRIAÇÃO DE DIRETÓRIOS
-        //Files.createDirectories(path.getParent());
-
-=======
     public static void main(String[] args) {
         Path path = Paths.get("C:/Users/Caio Oliveira/Documents/cursoJavaXTI/part3/texto.txt");
         Charset utf8 = StandardCharsets.UTF_8;
@@ -57,6 +35,5 @@ public class Arquivo {
         } catch (IOException e) {
             e.printStackTrace();
         }
->>>>>>> de702cc5571a04cba038bc8616481ef3cf474c67
     }
 }
